5989e57cfc7698c6c52c86561f59b6bf149a990e
sneethu <neethu.som.ram@gmail.com>
2014-10-11 19:20:01 +0100

    comment 2 remote

--- src/main/java/org/springframework/integration/samples/helloworld/HelloService.java
+++ src/main/java/org/springframework/integration/samples/helloworld/HelloService.java
@@ -24,6 +24,7 @@ package org.springframework.integration.samples.helloworld;
 public class HelloService {
 
 	public String sayHello(String name) {
+		/* comment 2 remote */
 		return "Hello " + name;
 	}
 
